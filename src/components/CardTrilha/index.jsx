405f55cad7db0b5165050dee11c7884d5ce01a1d
Carina Felippus <carifel@gmail.com>
2024-03-29 15:20:51 -0300

    Adição de Props

--- src/components/CardTrilha/index.jsx
+++ src/components/CardTrilha/index.jsx
@@ -1,9 +1,27 @@
-function CardTrilha(){
+import * as PropTypes from "prop-types"
+
+function CardTrilha({dadosTrilha}){
     return(
         <>
-            <h1>Card trilha</h1>
+            <h1>{dadosTrilha.nomeTrilha}</h1>
+            <span>{dadosTrilha.cidade} / {dadosTrilha.estado}</span>
+            <img width = {200} src={dadosTrilha.urlImagem} alt="imagem Trilha" />
         </>
     )
+    //Configuração das PropTypes
+    CardTrilha.propTypes = {
+        dadosTrilha: PropTypes.exact({
+            nomeTrilha: PropTypes.string.isRequired,
+            cidade: PropTypes.string.isRequired,
+            estado: PropTypes.string.isRequired,
+            duracao: PropTypes.number.isRequired,
+            trajeto: PropTypes.number.isRequired,
+            dificuldade: PropTypes.string.isRequired,
+            tipo: PropTypes.oneOf(['caminhada / trekking', 'ciclismo']),
+            nomeUsuario: PropTypes.string.isRequired,
+            urlImagem: PropTypes.string.isRequired
+        })
+    }
 }
 
 export default CardTrilha;
