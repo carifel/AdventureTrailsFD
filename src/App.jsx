7214701ecc36e1d58ec448624224e525fd2c6fd5
Carina Felippus <carifel@gmail.com>
2024-03-29 15:49:09 -0300

    Estilização

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import CardTrilha from "./components/CardTrilha"
+import "./App.css"
+
 function App() {
 
   const listaTrilhas = [
@@ -16,14 +18,15 @@ function App() {
   ]
 
   return (
-    <>
+    <div className="container">
+      <h1 className="titulo">Explore trilhas incríveis</h1>
     {
       listaTrilhas.map((trilha, index) =>(
         <CardTrilha dadosTrilha={trilha} key={index}/>
       ))
     }
       
-    </>
+    </div>
   )
 }
 
